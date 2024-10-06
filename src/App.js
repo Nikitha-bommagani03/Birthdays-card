bcba3d2d4d7f5c9e15f5b33efe06c7cfefa70f8d
Nikitha-bommagani03 <nikithabommagani03@gmail.com>
2024-10-06 21:45:02 +0530

    Birthday card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './db';
+import "./Style.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let App = () => {
+    let [state, setState] = useState(data);
+
+    return (
+        <div id='container'>
+            <h1 id='head'>{state.length} Birthdays Today</h1>
+            <div id='pic'>
+                {state.map(x => {
+                    return (
+                        <div key={x.id} id='box'>
+                            <img src={x.image} alt="picture" className="birthday-image" />
+                            <ruby id='name'>{x.name}<br/><rt>{x.age} years</rt></ruby>
+                        </div>
+                    );
+                })}
+            </div>
+            <div id='but'>
+                <button onClick={Clear} className="clear-button">Clear All</button>
+            </div>
+        </div>
+    );
+
+    function Clear() {
+        setState([]);
+    }
 }
 
 export default App;
